13e34f87cf521a86cda1d37a50a17428a4d99fe5
Omar Jabour <jabour.omar@hotmail.com>
2026-05-18 17:27:37 +0200

    working on the syntaxe .

--- 01-Syntaxe/01-syntaxe.sql
+++ 01-Syntaxe/01-syntaxe.sql
@@ -369,4 +369,111 @@ SELECT MAX(salaire) FROM employes;
 |         5000 |
 +--------------+
 
+SELECT *
+FROM employes
+WHERE service = "production"
+AND salaire IN (1900, 2300);
 
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+-- EXERCICES :
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+-- 1 -- Afficher la profession de l'employé 547.
+
+SELECT service
+FROM employes
+WHERE id_employes = 547;
+
+-- 2 -- Afficher la date d'embauche d'Amandine.
+SELECT date_embauche
+FROM employes
+WHERE prenom = "Amandine";
+
+-- 3 -- Afficher le nom de famille de Guillaume	SELECT nom
+FROM employes
+WHERE prenom = "Guillaume";
+
+
+-- 4 -- Afficher le nombre de personne ayant un n° id_employes commençant par le chiffre 5.	
+SELECT COUNT(*)
+FROM employes
+WHERE id_employes LIKE "5%";
+
+
+-- 5 -- Afficher le nombre de commerciaux.
+SELECT COUNT(*)
+FROM employes
+WHERE service = "commercial";
+
+
+-- 6 -- Afficher le salaire moyen des informaticiens SELECT AVG(salaire)
+SELECT AVG(salaire)
+FROM employes
+WHERE service = "informatique";
+
+
+-- 7 -- Afficher les 5 premiers employés après avoir classé leur nom de famille par ordre alphabétique. 
+SELECT *
+FROM employes
+ORDER BY nom ASC
+LIMIT 5;
+
+
+-- 8 -- Afficher le coût des commerciaux sur 1 année.	
+SELECT SUM(salaire * 12)
+FROM employes
+WHERE service = "commercial";
+
+
+-- 9 -- Afficher le salaire moyen par service.
+SELECT service, AVG(salaire)
+FROM employes
+GROUP BY service;
+
+-- 10 -- Afficher le nombre de recrutement sur l'année 2010
+SELECT COUNT(*)
+FROM employes
+WHERE YEAR(date_embauche) = 2010;
+
+
+-- 11 -- Afficher le salaire moyen appliqué lors des recrutements sur la période allant de 2015 a 2017
+SELECT AVG(salaire) FROM employes WHERE date_embauche BETWEEN "2015-01-01" AND "2017-12-31";
+
+
+-- 12 -- Afficher le nombre de service différent 
+SELECT COUNT(DISTINCT service) FROM employes;
+
+
+-- 13 -- Afficher tous les employés sauf ceux du service production et secrétariat
+SELECT * FROM employes WHERE service NOT IN ("production", "secretariat");
+
+-- 14 -- Afficher conjointement le nombre d'homme et de femme dans l'entreprise
+SELECT sexe, COUNT(*) FROM employes GROUP BY sexe;
+
+
+-- 15 -- Afficher les commerciaux ayant été recrutés avant 2012 de sexe masculin et gagnant un salaire supérieur a 2500 €
+SELECT * FROM employes WHERE service = "commercial" AND sexe = "m" AND date_embauche < "2012-01-01" AND salaire > 2500;
+
+
+-- 16 -- Qui a été embauché en dernier 
+SELECT * FROM employes ORDER BY date_embauche DESC LIMIT 1;
+
+
+-- 17 -- Afficher les informations sur l'employé du service commercial gagnant le salaire le plus élevé 
+SELECT * FROM employes WHERE service = "commercial" ORDER BY salaire DESC LIMIT 1;
+
+
+-- 18 -- Afficher le prénom du comptable gagnant le meilleur salaire
+SELECT prenom FROM employes WHERE service = "comptabilite" ORDER BY salaire DESC LIMIT 1;
+-- 19 -- Afficher le prénom de l'informaticien ayant été recruté en premier 
+SELECT prenom FROM employes WHERE service = "informatique" ORDER BY date_embauche ASC LIMIT 1;
+
+
+-- 20 -- Augmenter chaque employé de 100  
+UPDATE employes
+SET salaire = salaire + 100;
+
+-- 21 -- Supprimer les employés du service secrétariat
+DELETE FROM employes
+WHERE service = "secretariat";
