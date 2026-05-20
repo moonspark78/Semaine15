faee507c3ea2d0f460cda56cb714603b7e2c4537
Omar Jabour <jabour.omar@hotmail.com>
2026-05-20 11:07:04 +0200

    working on the union /./.

---
+++ 05-transaction/05-transaction.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+--------------- TRANSACTIONS ---------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+
+
+-- Les transactions en MySQL sont possibles uniquement avec le motteur InnoDB 
+-- Les transactions permettent de créer un espace tampon/de stagging pour le lancement de nos requêtes
+-- C'est à dire que, lorsque j'ouvre une transaction, une requête n'est réellement validée sur la base que lorsque je "commit" la transaction
+-- D'ailleurs, on pourra aussi annuler toutes les requêtes exécutées dans une transaction avec un "rollback"
+
+-- On s'en sert souvent dans le web lorsqu'une opération est composée de plusieurs requêtes qui sont liées. En lançant une transaction on s'assure de bien valider les deux, et que s'il y a un problème, d'annuler les deux ! (deux ou plus)
+
+-- On les utilise généralement au travers de notre langage back (via PDO en PHP) mais c'est un outil existant déjà dans MySQL que je peux manipuler avec ma console 
+
+USE entreprise;
