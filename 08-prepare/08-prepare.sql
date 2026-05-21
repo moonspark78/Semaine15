23a4acdea76c65e4c3e33127078084684676eedf
Omar Jabour <jabour.omar@hotmail.com>
2026-05-21 09:42:19 +0200

    working on the transaction sql ./././

--- 08-prepare/08-prepare.sql
+++ 08-prepare/08-prepare.sql
@@ -9,3 +9,9 @@
 -- Elle va passer par un cycle d'analyse, d'interprétation puis d'exécution 
 
 -- A quoi ça sert ? On s'en sert pour la sécurité ! Une requête préparée va être de se construire, de filtrer les params qu'on lui envoie, avant de s'exécuter 
+
+
+SELECT * FROM employes WHERE prenom = "Jean-Pierre";
+
+-- Là je défini une variable supposée reçu d'un form, d'une saisie utilisateur
+SET @prenom = "Jean-Pierre";
