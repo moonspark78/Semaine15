63c8a69421a1a55a7ffc096c498e1531e95b5304
Omar Jabour <jabour.omar@hotmail.com>
2026-05-21 09:42:06 +0200

    working on the transaction sql ./././.

--- 08-prepare/08-prepare.sql
+++ 08-prepare/08-prepare.sql
@@ -3,3 +3,9 @@
 --------------- REQUETE PREPAREES ----------------------------------------------------------
 --------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------
+
+-- Une requête préparée c'est une requête qui ne se lance pas immédiatement ! 
+
+-- Elle va passer par un cycle d'analyse, d'interprétation puis d'exécution 
+
+-- A quoi ça sert ? On s'en sert pour la sécurité ! Une requête préparée va être de se construire, de filtrer les params qu'on lui envoie, avant de s'exécuter 
