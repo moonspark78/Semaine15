472083faa82ec5fccaa8ed1bc746b0abb9c85cb2
Omar Jabour <jabour.omar@hotmail.com>
2026-05-22 14:30:54 +0200

    working on the crypto exercices  ././

---
+++ 10-Cryptographie/exoCrypto/exo04-signature.php
@@ -0,0 +1,10 @@
+<?php 
+
+/*
+
+Contexte
+
+Le principe de signature numérique est souvent utilisé avec des fichiers. Voici le processus :
+
+    Création de la signature :
+    On génère une signature à partir du contenu du fichier avec une clé privée.
