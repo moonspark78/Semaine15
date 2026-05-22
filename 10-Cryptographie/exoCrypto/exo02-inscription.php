216099e5b331345a90d5b72f3f2f6002f0053b79
Omar Jabour <jabour.omar@hotmail.com>
2026-05-22 14:26:47 +0200

    working on the crypto exercices  .././.

--- 10-Cryptographie/exoCrypto/exo02-inscription.php
+++ 10-Cryptographie/exoCrypto/exo02-inscription.php
@@ -16,3 +16,11 @@
                 - Créer un IV aléatoire en cumulant random_bytes() puis openssl_cipher_iv_length()
                 - Crypter ensuite la donnée avec openssl_encrypt() en appliquant bien l'IV
                 - La retransformer en base64 avant de la stocker base64_encode
+                - Insérer en BDD 
+            - Lorsque tout est OK et que l'insertion s'effectue convenablement (vérifier les données dans PHPMyAdmin, password hash et email chiffré), mettre en place une redirection vers une page indiquant à l'utilisateur qu'il est convenablement incrit, et lui respécifier son email (il faudra le dechiffrer)
+                - Redécoder de base64 l'email récupéré en BDD
+                - A partir de là, la chaine récupérée contient l'IV en debut de chaine (celui appliqué au chiffrement, il va falloir le découper avec substr pour extraire à la fois l'iv et à la fois le reste de la chaine cryptée)
+                - Une fois la découpe des deux éléments faites, utiliser openssl_decrypt  pour déchiffrer la donnée 
+
+
+                */
