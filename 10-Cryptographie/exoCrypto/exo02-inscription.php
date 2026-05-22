4ac7ab937015d4387667a4e5e6dd15780f59d53d
Omar Jabour <jabour.omar@hotmail.com>
2026-05-22 14:26:14 +0200

    working on the crypto exercices  ../././.

---
+++ 10-Cryptographie/exoCrypto/exo02-inscription.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+
+    EXERCICE Chiffrement de données sensibles et hash du password :
+
+        Etapes : 
+            - Reprendre l'exercice POST Inscription Utilisateur du dossier PHP - Chapitre Post - Exercices
+            - Modifier le code pour le faire correspondre à une réelle insertion en bdd (base "crypto"  table "membre"  id_membre, pseudo, email, password)
+            - Bien gérer la vérification du doublon en table
+            - Gérer la sécurité de l'insertion
+            
