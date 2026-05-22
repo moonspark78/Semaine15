cec758a42513be5348ca489aaa73d966784be182
Omar Jabour <jabour.omar@hotmail.com>
2026-05-22 14:31:08 +0200

    working on the crypto exercices  ./.

--- 10-Cryptographie/exoCrypto/exo04-signature.php
+++ 10-Cryptographie/exoCrypto/exo04-signature.php
@@ -8,3 +8,10 @@ Le principe de signature numérique est souvent utilisé avec des fichiers. Voic
 
     Création de la signature :
     On génère une signature à partir du contenu du fichier avec une clé privée.
+        Stockage :
+    La signature est généralement sauvegardée dans un fichier séparé ou transmise avec le fichier original.
+
+    Vérification :
+    À l'arrivée, l'utilisateur récupère le fichier original et la signature. Avec la clé publique associée, il vérifie que la signature correspond bien au contenu.
+
+Si le contenu est modifié, la vérification échoue.
