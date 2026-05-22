7cffd36acb9fdbd021211f33338c758a27f9047a
Omar Jabour <jabour.omar@hotmail.com>
2026-05-22 14:24:16 +0200

    working on the crypto exercices  ../././././.

--- 10-Cryptographie/exoCrypto/exo01-cmdLine.php
+++ 10-Cryptographie/exoCrypto/exo01-cmdLine.php
@@ -6,3 +6,11 @@
 // Installation de OpenSSL, via setup sur diverse distribution : 
 - https://slproweb.com/products/Win32OpenSSL.html 
 - https://thesecmaster.com/blog/procedure-to-install-openssl-on-the-windows-platform
+
+// Sur Mac via homebrew 
+- https://brew.sh/
+- https://formulae.brew.sh/formula/openssl@3
+
+// Sur Linux, à la mano
+sudo apt update
+sudo apt install openssl
