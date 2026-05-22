4d5aafd3c533249e231243e6fbe3ed991085f663
Omar Jabour <jabour.omar@hotmail.com>
2026-05-22 14:24:56 +0200

    working on the crypto exercices  .././././

--- 10-Cryptographie/exoCrypto/exo01-cmdLine.php
+++ 10-Cryptographie/exoCrypto/exo01-cmdLine.php
@@ -24,3 +24,13 @@ sudo apt install openssl
     // chemin d'accès (pour moi)  : C:\wamp64\bin\apache\apache2.4.59\conf\openssl.cnf
 
 
+
+// Exercice 01 - Chiffrer et déchiffrer un fichier .txt en ligne de commande 
+
+    Etapes : 
+
+        1 - Créez un fichier confidentiel.txt contenant un message.
+        2 - Chiffrez avec la commande openssl enc -e   (plusieurs options sont à définir, à votre propre choix, un algorithme de cryptage, il faut aussi indiquer le fichier -in et le fichier -out)
+        3 - Déchiffrer avec openssl enc -d 
+
+*/
