1db07aec7a1ac2898146df1b72b2f559c5ae2fe1
Omar Jabour <jabour.omar@hotmail.com>
2026-05-22 14:31:21 +0200

    working on the crypto exercices  ./

--- 10-Cryptographie/exoCrypto/exo04-signature.php
+++ 10-Cryptographie/exoCrypto/exo04-signature.php
@@ -15,3 +15,20 @@ Le principe de signature numérique est souvent utilisé avec des fichiers. Voic
     À l'arrivée, l'utilisateur récupère le fichier original et la signature. Avec la clé publique associée, il vérifie que la signature correspond bien au contenu.
 
 Si le contenu est modifié, la vérification échoue.
+
+        EXERCICE : Signature d'un fichier 
+
+        Etapes : 
+            - Réutiliser les clés privée/publique de l'exercice précédent 
+            - Ajouter un document pdf quelconque à votre dossier
+            - Créer une signature avec openssl_sign 
+            - Enregistrer la signature dans un fichier 
+            - Afficher un message 
+
+            - Créez un dossier à l'intérieur du dossier exercices 
+            - Ajouter une copie de votre doc pdf et du fichier contenant la signature dans ce dossier
+            - Récupérer la clé publique de l'exercice précédent ainsi que la signature qui a été créée par le premier fichier 
+            - Faire une vérification du document de la signature avec openssl_verify
+            - Afficher un message pour indiquer que la signature est valide ou invalide
+
+*/
