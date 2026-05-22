b27668ac9f8a9d7750572eb6d846753ce4c9bc1f
Omar Jabour <jabour.omar@hotmail.com>
2026-05-22 14:23:59 +0200

    working on the crypto exercices  .

---
+++ 10-Cryptographie/exoCrypto/exo01-cmdLine.php
@@ -0,0 +1,8 @@
+<?php 
+
+/*
+// Chiffrer un fichier via OpenSSL et le terminal 
+
+// Installation de OpenSSL, via setup sur diverse distribution : 
+- https://slproweb.com/products/Win32OpenSSL.html 
+- https://thesecmaster.com/blog/procedure-to-install-openssl-on-the-windows-platform
